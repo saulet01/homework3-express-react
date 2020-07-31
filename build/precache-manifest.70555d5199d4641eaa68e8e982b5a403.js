3780a9a7e3b46a125694b92e617e5869691b63f3
Saulet Yskak <sauletik01@gmail.com>
2020-07-31 11:41:57 +0600

    first init

---
+++ build/precache-manifest.70555d5199d4641eaa68e8e982b5a403.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917a9d62ca73ebadb6cf747b3ded5acc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4622e4abcee8a5cf8c67",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4622e4abcee8a5cf8c67",
+    "url": "/static/js/main.a36095c8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
